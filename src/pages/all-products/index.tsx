90b002a3a45f0f4690657bdd383c4efaae405b07
armagansenol <armagansnl@gmail.com>
2023-10-25 15:30:18 +0300

    update fetch logic

--- src/pages/all-products/index.tsx
+++ src/pages/all-products/index.tsx
@@ -1,98 +1,112 @@
-import { Fragment, useState } from "react"
+import { useState } from "react"
 import s from "./all-products.module.scss"
 
+import cn from "clsx"
 import { Link } from "react-router-dom"
 
-import cakesBottomPeanut from "@/assets/img/cakes-bottom-peanut.png"
-import cakesTopPeanut from "@/assets/img/cakes-top-peanut.png"
-
-import chocolate from "@/assets/img/crumby-chocolate.png"
-import darkChocolate from "@/assets/img/crumby-dark-chocolate.png"
-import peanut from "@/assets/img/crumby-peanut.png"
-import strawberry from "@/assets/img/crumby-strawberry.png"
-
+import { useAll } from "@/api-client/queries"
 import CallToContact from "@/components/call-to-contact"
 import CardProduct from "@/components/card-product"
-import Filter from "@/components/sort"
-import Searchbox from "@/components/searchbox"
-import Products from "@/layouts/products"
 import LoadingScreen from "@/components/loading-screen"
-import { useAll } from "@/api-client/queries"
+import Searchbox from "@/components/searchbox"
+import Filter from "@/components/sort"
+import Products from "@/layouts/products"
+import { Option } from "@/global"
 
 const AllProducts = () => {
-  const products = [
-    <Link to={`/all-products/detail`}>
-      <CardProduct
-        img={chocolate}
-        hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
-        name={
-          <>
-            MILK CHOCOLATE <br />+ PEANUT
-          </>
-        }
-      />
-    </Link>,
-    <Link to={`/all-products/detail`}>
-      <CardProduct
-        img={peanut}
-        hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
-        name={
-          <>
-            MILK CHOCOLATE <br />+ DRIED STRAWBERRY
-          </>
-        }
-      />
-    </Link>,
-    <Link to={`/all-products/detail`}>
-      <CardProduct
-        img={strawberry}
-        hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
-        name={<>MILK CHOCOLATE</>}
-      />
-    </Link>,
-    <Link to={`/all-products/detail`}>
-      <CardProduct
-        img={darkChocolate}
-        hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
-        name={<>DARK CHOCOLATE</>}
-      />
-    </Link>,
-  ]
+  // const products = [
+  //   <Link to={`/all-products/detail`}>
+  //     <CardProduct
+  //       img={chocolate}
+  //       hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
+  //       name={
+  //         <>
+  //           MILK CHOCOLATE <br />+ PEANUT
+  //         </>
+  //       }
+  //     />
+  //   </Link>,
+  //   <Link to={`/all-products/detail`}>
+  //     <CardProduct
+  //       img={peanut}
+  //       hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
+  //       name={
+  //         <>
+  //           MILK CHOCOLATE <br />+ DRIED STRAWBERRY
+  //         </>
+  //       }
+  //     />
+  //   </Link>,
+  //   <Link to={`/all-products/detail`}>
+  //     <CardProduct
+  //       img={strawberry}
+  //       hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
+  //       name={<>MILK CHOCOLATE</>}
+  //     />
+  //   </Link>,
+  //   <Link to={`/all-products/detail`}>
+  //     <CardProduct
+  //       img={darkChocolate}
+  //       hoverImg={{ top: cakesTopPeanut, bottom: cakesBottomPeanut }}
+  //       name={<>DARK CHOCOLATE</>}
+  //     />
+  //   </Link>,
+  // ]
 
-  const [keyword, setKeyword] = useState("")
-  const [sort, setSort] = useState("")
+  const [keyword, setKeyword] = useState<string | null>(null)
+  const [sort, setSort] = useState<Option | null>(null)
 
-  const { isLoading } = useAll("a", "b")
-
-  if (isLoading) {
-    return <LoadingScreen />
-  }
+  const { isLoading, data } = useAll(keyword, sort)
 
   return (
     <Products>
-      <main className={s.allProducts}>
-        <section className={s.filterC}>
-          <Searchbox keyword={keyword} setKeyword={setKeyword} />
-          <Filter
-            label="SORT"
-            options={[
-              { label: "NEWEST FIRST", value: "NEWEST_FIRST" },
-              { label: "FILTER 2", value: "desc" },
-              { label: "FILTER 3", value: "th" },
-            ]}
-            sort={sort}
-            setSort={setSort}
-          />
-        </section>
-        <section className="flex-center-y">
-          <div className={s.productsList}>
-            {products.map((product, i) => {
-              return <Fragment key={i}>{product}</Fragment>
-            })}
-          </div>
-        </section>
-        <CallToContact />
-      </main>
+      <section className={s.filterC}>
+        <Searchbox keyword={keyword} setKeyword={setKeyword} />
+        <Filter
+          label="SORT"
+          options={[
+            { label: "NEWEST FIRST", value: "NEWEST_FIRST" },
+            { label: "FILTER 2", value: "desc" },
+            { label: "FILTER 3", value: "th" },
+          ]}
+          sort={sort}
+          setSort={setSort}
+        />
+      </section>
+
+      <>
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            {data ? (
+              <section className="flex-center-y">
+                <div className={s.productsList}>
+                  {data &&
+                    data.map((product) => {
+                      return (
+                        <Link to={`/all-products/${product.url}`} key={product.id}>
+                          <CardProduct
+                            img={product.img}
+                            hoverImg={{ top: product.hoverImg.top, bottom: product.hoverImg.bottom }}
+                            name={product.name}
+                          />
+                        </Link>
+                      )
+                    })}
+                </div>
+              </section>
+            ) : (
+              <div className={cn(s.notFound, "flex-center")}>
+                <h1>Oops!</h1>
+                <p>The products you are looking for were not found.</p>
+              </div>
+            )}
+          </>
+        )}
+      </>
+
+      <CallToContact />
     </Products>
   )
 }
